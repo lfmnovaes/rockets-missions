64fec1bbdbe9e48d5bb20c12d93a6b3488b3b697
mustabbas2000@gmail.com <mustabbas2000@gmail.com>
2021-11-10 02:12:56 +0300

    Add profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,25 @@
-const Profile = () => (
-  <h1>Profile</h1>
-);
+import React from 'react';
+import { useSelector } from 'react-redux';
+import '../App.css';
+
+
+const Profile = () => {
+
+  const rocketlist = useSelector((data) => data.rocketsReducer);
+
+  const rocketItem = rocketlist.filter((item) => item.reserved).map((item) => {
+    return(
+        <div className ="profile">
+        <p>{item.rocket_name}</p>
+        </div>
+    );
+  })
+  return(
+    <div>
+      <h3>My Rockets</h3>
+        {rocketItem} 
+    </div>
+  );
+}
 
 export default Profile;
