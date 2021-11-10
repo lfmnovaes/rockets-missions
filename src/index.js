9aa73cd4ca88aa88d924803d1baa7766814c6ac5
mustabbas2000@gmail.com <mustabbas2000@gmail.com>
2021-11-10 20:47:59 +0300

    Fix linters

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 
-import store from './Redux/cofigureStore';
+import store from './Redux/configureStore';
 
 ReactDOM.render(
   <Provider store={store}>
