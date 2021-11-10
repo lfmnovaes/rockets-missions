0c038eb70c90bd349e422ce5f47cc11bc3a3f45c
Luis <lfmnovaes@gmail.com>
2021-11-10 16:25:04 -0300

    Initial changes to join/leave mission

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,17 +1,31 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Button } from 'react-bootstrap';
+import { joinMission, leaveMission } from '../redux/missions/missions';
 
 export default function Mission({ data }) {
+  const dispatch = useDispatch();
+
+  const missionClick = (id, status = false) => {
+    if (status === false) {
+      dispatch(joinMission(id));
+    } else {
+      dispatch(leaveMission(id));
+    }
+  };
+
   return (
     <tr>
       <th>
         {data.mission_name}
       </th>
       <th>
-        {data.mission_id}
+        {data.description}
       </th>
       <th>
-        {data.description}
+        <Button onClick={() => missionClick(data.mission_id, data.status)} />
+        {data.status}
       </th>
     </tr>
   );
@@ -19,8 +33,9 @@ export default function Mission({ data }) {
 
 Mission.propTypes = {
   data: PropTypes.shape({
-    mission_name: PropTypes.string,
     mission_id: PropTypes.string,
+    mission_name: PropTypes.string,
     description: PropTypes.string,
+    status: PropTypes.bool,
   }).isRequired,
 };
--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,5 +1,5 @@
-const ADD_MISSION = 'spaceHub/missions/ADD_MISSION';
-const REMOVE_MISSION = 'spaceHub/missions/REMOVE_MISSION';
+const JOIN_MISSION = 'spaceHub/missions/JOIN_MISSION';
+const LEAVE_MISSION = 'spaceHub/missions/LEAVE_MISSION';
 const GET_MISSIONS = 'spaceHub/missions/GET_MISSIONS';
 
 const API_URL = 'https://api.spacexdata.com/v3/missions';
@@ -13,25 +13,38 @@ export const getMissions = () => async (dispatch) => {
     .then((data) => {
       missions = data;
     });
-  console.log('missions', missions);
   dispatch({
     type: GET_MISSIONS,
     payload: missions,
   });
 };
 
+export const joinMission = (payload) => ({
+  type: JOIN_MISSION,
+  payload,
+});
+
+export const leaveMission = (payload) => ({
+  type: LEAVE_MISSION,
+  payload,
+});
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_MISSION:
-      // postMission(action.payload);
-      return [
-        ...state,
-        action.payload,
-      ];
-    case REMOVE_MISSION:
-      // deleteMission(action.payload);
-      // return state.filter((mission) => mission.id !== action.payload);
-      return state;
+    case JOIN_MISSION:
+      return state.map((mission) => {
+        if (mission.mission_id === action.payload) {
+          return { ...mission, reserved: true };
+        }
+        return mission;
+      });
+    case LEAVE_MISSION:
+      return state.map((mission) => {
+        if (mission.mission_id === action.payload) {
+          return { ...mission, reserved: false };
+        }
+        return mission;
+      });
     case GET_MISSIONS:
       return action.payload;
     default:
